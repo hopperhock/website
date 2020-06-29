ec0be5120144d8e65a5dab3fec377168aec9d525
unknown <amttdev@gmail.com>
2020-06-28 20:52:42 -0600

    chore: Implemented the color palette and edited the theme interface to include more color shades.

--- src/styles/utilities/colors.ts
+++ src/styles/utilities/colors.ts
@@ -1,22 +1,64 @@
-export const CINNABAR = '#DC3534';
-export const APPLE_BLOSSOM = '#C3011A';
-export const CHATEAU_GREEN = '#28A745';
-export const BLACK = '#393939';
-export const WHITE = '#ffffff';
-export const SOLITUDE = '#e9ecef';
+export enum BLUE {
+  d1 = '#003E6B',
+  d2 = '#0A558C',
+  d3 = '#0F609B',
+  d4 = '#186FAF',
+  d5 = '#2680C2',
+  l5 = '#4098D7',
+  l4 = '#62B0E8',
+  l3 = '#84C5F4',
+  l2 = '#B6E0FE',
+  l1 = '#DCEEFB',
+}
 
-export const CUTTY_SARK = '#557177';
-export const POSEIDON = '#134E5C';
-export const POSEIDON_REGRESION = '#036981';
-export const DEEP_SEA_GREEN = '#0A4957';
-export const TEAL_BLUE = '#034352';
-export const TEAL_BLUE_DARK = '#023D4A';
-export const TIBER = '#0A2F37';
-export const BLACK_PEARL = '#03151A';
-export const DIM_GRAY = '#646464';
+export enum BLUE_GREY {
+  d1 = '#102A43',
+  d2 = '#243B53',
+  d3 = '#334E68',
+  d4 = '#486581',
+  d5 = '#627D98',
+  l5 = '#829AB1',
+  l4 = '#9FB3C8',
+  l3 = '#BCCCDC',
+  l2 = '#D9E2EC',
+  l1 = '#F0F4F8',
+}
 
-export const ROPE = '#95581B';
-export const TENN = '#D06700';
-export const KORMA = '#8D4C0D';
-export const PERU_TAN = '#854201';
-export const CINNAMON = '#783C00';
+export enum TEAL {
+  d1 = '#004440',
+  d2 = '#016457',
+  d3 = '#048271',
+  d4 = '#079A82',
+  d5 = '#17B897',
+  l5 = '#2DCCA7',
+  l4 = '#5FE3C0',
+  l3 = '#8EEDD1',
+  l2 = '#C6F7E9',
+  l1 = '#F0FCF9',
+}
+
+export enum RED {
+  d1 = '#610404',
+  d2 = '#780A0A',
+  d3 = '#911111',
+  d4 = '#A61B1B',
+  d5 = '#BA2525',
+  l5 = '#D64545',
+  l4 = '#E66A6A',
+  l3 = '#F29B9B',
+  l2 = '#FACDCD',
+  l1 = '#FFEEEE',
+}
+
+export enum YELLOW {
+  d1 = '#513C06',
+  d2 = '#7C5E10',
+  d3 = '#A27C1A',
+  d4 = '#C99A2E',
+  d5 = '#E9B949',
+  l5 = '#F7D070',
+  l4 = '#F9DA8B',
+  l3 = '#F8E3A3',
+  l2 = '#FCEFC7',
+  l1 = '#FFFAEB',
+}
