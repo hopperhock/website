4c3b95c3d02290841d908294987a222c8cbbd3c2
unknown <amttdev@gmail.com>
2020-07-05 17:03:37 -0600

    chore: Replace the font sizes naming convention.

--- src/styles/utilities/font-sizes.ts
+++ src/styles/utilities/font-sizes.ts
@@ -1,17 +1,25 @@
-export const TITLE_1: string = '2.5rem';
-export const TITLE_2: string = '2.25rem';
-export const TITLE_3: string = '2rem';
-export const SUBTITLE_1: string = '1.625rem';
-export const SUBTITLE_2: string = '1.5rem';
-export const SUBTITLE_3: string = '1.375rem';
-export const SUBTITLE_4: string = '1.25rem';
-export const TEXT_BODY_1: string = '1.125rem';
-export const TEXT_BODY_2: string = '1rem';
-export const TEXT_BODY_3: string = '0.875rem';
-export const TEXT_BODY_4: string = '0.75rem';
-export const TEXT_BODY_5: string = '0.625rem';
+export enum Title {
+  SIZE_1 = '2.5rem',
+  SIZE_2 = '2.25rem',
+  SIZE_3 = '2rem'
+}
 
-export enum FONT_WEIGHT {
+export enum Subtitle {
+  SIZE_1 = '1.625rem',
+  SIZE_2 = '1.5rem',
+  SIZE_3 = '1.375rem',
+  SIZE_4 = '1.25rem'
+}
+
+export enum TextBody {
+  SIZE_1 = '1.125rem',
+  SIZE_2 = '1rem',
+  SIZE_3 = '0.875rem',
+  SIZE_4 = '0.75rem',
+  SIZE_5 = '0.625rem'
+}
+
+export enum FontWeight {
   'black' = 900,
   'bold' = 700,
   'medium' = 500,
