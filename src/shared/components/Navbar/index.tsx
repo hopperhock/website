df382eb8d40df4cf114039ba320c473382711ee4
unknown <amttdev@gmail.com>
2020-07-05 19:09:17 -0600

    feature: Finished navbar implementation

--- src/shared/components/Navbar/index.tsx
+++ src/shared/components/Navbar/index.tsx
@@ -1,18 +1,36 @@
-import React, { SFC } from 'react';
-import styled from 'styled-components';
+import React, { SFC, useState, useEffect } from 'react';
+import { Nav } from './navbar.styles';
+import HopperhockLogo from 'styles/illustrations/hopperhock-logo.svg';
+import Link from 'next/link';
 
 const Navbar: SFC = () => {
-  return <Nav>Navbar</Nav>;
+  const [isTransparent, setIsTransparent] = useState(true);
+  useEffect(() => {
+    document.addEventListener('scroll', () => {
+      const isScrolled = window.scrollY < 100;
+      setIsTransparent(isScrolled);
+    });
+  }, []);
+  return (
+    <Nav transparent={isTransparent} aria-transparent={isTransparent}>
+      <HopperhockLogo className="brand" />
+      <div className="nav-links">
+        <Link href="/about-us">
+          <a className="nav-link">Home</a>
+        </Link>
+        <Link href="/about-us">
+          <a className="nav-link">Services</a>
+        </Link>
+        <Link href="/about-us">
+          <a className="nav-link">About Us</a>
+        </Link>
+        <Link href="/about-us">
+          <a className="nav-link">Contact Us</a>
+        </Link>
+      </div>
+      <button className="nav-action">BOOK A CALL</button>
+    </Nav>
+  );
 };
-
-const Nav = styled.div`
-  background-color: ${({ theme }) => theme.PRIMARY_COLOR_DARK_1};
-  color: ${({ theme }) => theme.NAVIGATION_TEXT_COLOR};
-  height: 20%;
-  padding: 0 0.5rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
 
 export default Navbar;
