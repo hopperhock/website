edee464b42b36ffad20797c6754ecc19b1e076c1
unknown <amttdev@gmail.com>
2020-07-05 19:09:59 -0600

    chore: Moved the type declarations into a single file.

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -3,3 +3,9 @@ import { Theme } from 'styles/theme/types';
 declare module 'styled-components' {
   interface DefaultTheme extends Theme {}
 }
+
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.svg';
+declare module '*.gif';
