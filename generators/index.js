4ac494743295bb79b6a3121f774340206e11a822
Hersey Aguilar <herseyaguilar@outlook.com>
2020-08-01 12:18:18 -0600

    feat: add plop generators

---
+++ generators/index.js
@@ -0,0 +1,8 @@
+const promptDirectory = require('inquirer-directory');
+const componentGenerator = require('./component/index.js');
+
+
+module.exports = plop => {
+  plop.setPrompt('directory', promptDirectory);
+  plop.setGenerator('component', componentGenerator);
+};
