2a7fc985d1b8b474284ae1bce73130d60ea008d6
Hersey Aguilar <herseyaguilar@outlook.com>
2020-10-01 15:17:35 -0600

    feat: add internationalization to home page

--- src/modules/home/introduction/index.tsx
+++ src/modules/home/introduction/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { HomeIntroContainer, HomeIntroDescription, HomeIntroVideo } from './home-introduction.styles';
 import { Images, HOME_VIDEO } from 'shared/constants/assets';
+import { useTranslation } from 'shared/utils/internationalization';
 
 const HomeIntroduction = () => {
+  const { t } = useTranslation('home');
+
   return (
     <HomeIntroContainer>
       <HomeIntroDescription>
-        <h1>WE ARE HOPPERHOCK.</h1>
-        <p>Custom Software Solutions That Empower Your Business.</p>
+        <h1>{t('header.title')}</h1>
+        <p>{t('header.content')}</p>
       </HomeIntroDescription>
       <HomeIntroVideo poster={Images.HOME_VIDEO_POSTER} autoPlay muted loop>
         <source src={HOME_VIDEO} type="video/mp4" />
