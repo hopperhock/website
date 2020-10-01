140b2ce82c5215107b7a842fe8ae5df1e6ca30ab
Hersey Aguilar <herseyaguilar@outlook.com>
2020-10-01 17:44:57 -0600

    feat: add internationalization to contact us page

--- src/modules/contact-us/contact-information/index.tsx
+++ src/modules/contact-us/contact-information/index.tsx
@@ -1,35 +1,37 @@
 import React, { FC } from 'react';
 import { Container, ContactUsInformationTitle, ContactTitle, ContactData, DataContainer, DataSection } from './styles';
-import * as tranlations from './Translations/en.json';
 import SocialNetworkDisplayer from 'shared/components/SocialNetworkDisplayer';
 import { SOCIAL_NETWORK_ICONS } from 'shared/constants/assets';
+import { useTranslation } from 'shared/utils/internationalization';
 
 const ContactInformation: FC = () => {
+  const { t } = useTranslation('contact-us');
+
   return (
     <Container>
       <ContactUsInformationTitle>
-        <span> {tranlations['contact.information.title']}</span>
+        <span> {t('information.title')}</span>
       </ContactUsInformationTitle>
       <DataContainer>
         <DataSection>
           <ContactTitle>
             <span />
-            <h1>{tranlations['contact.information.contactTitle']}</h1>
+            <h1>{t('information.contactTitle')}</h1>
           </ContactTitle>
-          <ContactData>{tranlations['contact.information.phone']}</ContactData>
-          <ContactData>{tranlations['contact.information.email']}</ContactData>
+          <ContactData>{t('information.phone')}</ContactData>
+          <ContactData>{t('information.email')}</ContactData>
         </DataSection>
         <DataSection>
           <ContactTitle>
             <span />
-            <h1>{tranlations['contact.information.addressTitle']}</h1>
+            <h1>{t('information.addressTitle')}</h1>
           </ContactTitle>
-          <ContactData>{tranlations['contact.information.address']}</ContactData>
+          <ContactData>{t('information.address')}</ContactData>
         </DataSection>
         <DataSection>
           <ContactTitle>
             <span />
-            <h1>{tranlations['contact.information.connectTitle']}</h1>
+            <h1>{t('information.connectTitle')}</h1>
           </ContactTitle>
           <SocialNetworkDisplayer socialLogos={SOCIAL_NETWORK_ICONS} />
         </DataSection>
