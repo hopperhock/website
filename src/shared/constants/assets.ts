198ceeef15f67d99e48a5a34cdf9e907e60e8089
Andres Matta <andres.matta@firstfactory.com>
2020-11-07 19:40:40 -0600

    fix: Changed the extensions of the assets.

--- src/shared/constants/assets.ts
+++ src/shared/constants/assets.ts
@@ -1,27 +1,27 @@
 export const HOME_VIDEO = 'https://hopperhock-ui-assets.s3.amazonaws.com/videos/HopperHockVideo.mp4';
 export enum Images {
-  HOME_VIDEO_POSTER = '/Images/VideoPoster.png',
-  SERVICE_IMAGE_CONTAINER = '/Images/ServiceMainContainer.png',
+  HOME_VIDEO_POSTER = '/Images/VideoPoster.webp',
+  SERVICE_IMAGE_CONTAINER = '/Images/ServiceMainContainer.webp',
   NS_COST = '/Images/Home/NSCost.svg',
   NS_CULTURE = '/Images/Home/NSCulture.svg',
   NS_QUALITY = '/Images/Home/NSQuality.svg',
   NS_TIME = '/Images/Home/NSTime.svg',
   NS_TRIPS = '/Images/Home/NSTrips.svg',
-  SERVICES_BANNER = '/Images/Services/ServicesBanner.png',
+  SERVICES_BANNER = '/Images/Services/ServicesBanner.webp',
   SERVICES_DESCRIPTION = '/Images/Services/ServicesDescription.svg',
   SERVICES_DESCRIPTION_MOBILE = '/Images/Services/ServicesDescriptionMobile.svg',
   PARALLAX_BANNER_CONTAINER = '/Images/BannerIrregularContainer.svg',
-  PARALLAX_BANNER_BACKGROUND = '/Images/BannerBackgroundImage.png',
-  SERVICE_WEB_BACKGROUND_WEB = '/Images/ServiceWebBackground.jpg',
-  SERVICE_WEB_BACKGROUND_UX = '/Images/ServiceUXBackground.png',
-  SERVICE_WEB_BACKGROUND_QA = '/Images/ServiceQABackground.jpeg',
-  STACK_SECTION_BACKGROUND = '/Images/TechnologyStackBackground.png',
+  PARALLAX_BANNER_BACKGROUND = '/Images/BannerBackgroundImage.webp',
+  SERVICE_WEB_BACKGROUND_WEB = '/Images/ServiceWebBackground.webp',
+  SERVICE_WEB_BACKGROUND_UX = '/Images/ServiceUXBackground.webp',
+  SERVICE_WEB_BACKGROUND_QA = '/Images/ServiceQABackground.wepb',
+  STACK_SECTION_BACKGROUND = '/Images/TechnologyStackBackground.webp',
   STACK_IMAGES_BASE_URL = '/Images/StackImages/',
   WORK_STEPS_BASE_URL = '/Images/Services/WorkSteps/',
-  ABOUT_US_INTRODUCTION = '/Images/AboutUs/Introduction/AboutUsIntroduction.svg',
-  FOUNDER_1 = '/Images/AboutUs/Founders/Founder1.jpg',
-  FOUNDER_2 = '/Images/AboutUs/Founders/Founder2.jpg',
-  FOUNDER_3 = '/Images/AboutUs/Founders/Founder3.jpg',
+  ABOUT_US_INTRODUCTION = '/Images/AboutUs/Introduction/AboutUsIntroduction.webp',
+  FOUNDER_1 = '/Images/AboutUs/Founders/Founder1.webp',
+  FOUNDER_2 = '/Images/AboutUs/Founders/Founder2.webp',
+  FOUNDER_3 = '/Images/AboutUs/Founders/Founder3.webp',
   FOUNDER_ICON = '/Images/AboutUs/Founders/UserIcon.svg',
   CB_DESKTOP = '/Images/Services/Capabilities/desktop.svg',
   CB_WEB = '/Images/Services/Capabilities/web.svg',
@@ -29,12 +29,12 @@ export enum Images {
   CB_BACKEND = '/Images/Services/Capabilities/backend.svg',
   CB_UX = '/Images/Services/Capabilities/ux.svg',
   CB_QA = '/Images/Services/Capabilities/qa.svg',
-  CONTACT_US_BANNER = '/Images/ContactUs/ContactUsbanner.png',
-  CONTACT_US_FORM = '/Images/ContactUs/ContactUsForm.png',
+  CONTACT_US_BANNER = '/Images/ContactUs/ContactUsbanner.webp',
+  CONTACT_US_FORM = '/Images/ContactUs/ContactUsForm.webp',
   SOCIAL_IMAGES_BASE_URL = '/Images/SocialImages/',
-  FOOTER_BACKGROUND = '/Images/FooterBackgroundImage.png',
-  FOOTER_SOCIAL_BACKGROUND = '/Images/FooterSocialBackgroundImage.png',
-  BRAND_WHITE = '/Images/BrandWhite.png',
+  FOOTER_BACKGROUND = '/Images/FooterBackgroundImage.webp',
+  FOOTER_SOCIAL_BACKGROUND = '/Images/FooterSocialBackgroundImage.webp',
+  BRAND_WHITE = '/Images/BrandWhite.webp',
 }
 
 export const STACK_IMAGES = [
@@ -42,7 +42,7 @@ export const STACK_IMAGES = [
   'java.svg',
   'spring.svg',
   'python.svg',
-  'nodejs.png',
+  'nodejs.svg',
   'typescript.svg',
   'javascript.svg',
   'react.svg',
@@ -56,21 +56,21 @@ export const STACK_IMAGES = [
 ];
 
 export var WORK_PROCESS_STEPS_EN = [
-  'EnProcess1.png',
-  'EnProcess3.png',
-  'EnProcess5.png',
+  'EnProcess1.webp',
+  'EnProcess3.webp',
+  'EnProcess5.webp',
   ,
-  'EnProcess2.png',
-  'EnProcess4.png',
+  'EnProcess2.webp',
+  'EnProcess4.webp',
 ];
 
 export var WORK_PROCESS_STEPS_ES = [
-  'EsProcess1.png',
-  'EsProcess3.png',
-  'EsProcess5.png',
+  'EsProcess1.webp',
+  'EsProcess3.webp',
+  'EsProcess5.webp',
   ,
-  'EsProcess2.png',
-  'EsProcess4.png',
+  'EsProcess2.webp',
+  'EsProcess4.webp',
 ];
 
 export var SOCIAL_NETWORK_ICONS = [
@@ -80,9 +80,4 @@ export var SOCIAL_NETWORK_ICONS = [
   { url: 'http://www.linkedin.com', image: '/Images/linkedin.svg' },
 ];
 
-export const SOCIAL_IMAGES = [
-  'twitter.svg',
-  'facebook.svg',
-  'instagram.svg',
-  'linkedin.svg'
-];
+export const SOCIAL_IMAGES = ['twitter.svg', 'facebook.svg', 'instagram.svg', 'linkedin.svg'];
